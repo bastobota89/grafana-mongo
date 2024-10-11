b8e42e96b31a561b028aea7c38cf771ea69d1c0b
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-10-12 01:26:47 +0600

    Updated plugin.json

--- src/datasource.ts
+++ src/datasource.ts
@@ -110,47 +110,4 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
             message: 'Success',
         });
     }
-
-    // async request(url: string, params?: string) {
-    //     const response = getBackendSrv().fetch<DataSourceResponse>({
-    //         url: `${this.url}${url}${params?.length ? `?${params}` : ''}`,
-    //     });
-    //     return lastValueFrom(response);
-    // }
-    //
-    // /**
-    //  * Checks whether we can connect to the API.
-    //  */
-    // async testDatasource() {
-    //     const defaultErrorMessage = 'Cannot connect to API';
-    //
-    //     try {
-    //         const response = await this.request('/health');
-    //         if (response.status === 200) {
-    //             return {
-    //                 status: 'success',
-    //                 message: 'Success',
-    //             };
-    //         } else {
-    //             return {
-    //                 status: 'error',
-    //                 message: response.statusText ? response.statusText : defaultErrorMessage,
-    //             };
-    //         }
-    //     } catch (err) {
-    //         let message = '';
-    //         if (typeof err === 'string') {
-    //             message = err;
-    //         } else if (isFetchError(err)) {
-    //             message = 'Fetch error: ' + (err.statusText ? err.statusText : defaultErrorMessage);
-    //             if (err.data && err.data.error && err.data.error.code) {
-    //                 message += ': ' + err.data.error.code + '. ' + err.data.error.message;
-    //             }
-    //         }
-    //         return {
-    //             status: 'error',
-    //             message,
-    //         };
-    //     }
-    // }
 }
